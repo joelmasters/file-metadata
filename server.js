e08a64f632e621561c90feceba584a7b6f78effa
Glitch (glistening-squid) <none>
2018-04-08 18:46:33 +0000

    🍉🔔 Checkpoint ./views/index.html:864828/33 ./public/client.js:864828/49 ./server.js:864828/340

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var fs = require('fs');
 var express = require('express');
 var app = express();
 
+app.set('json spaces', 2);
 app.use(express.static('public'));
 
 app.get('/', function(req, res) {
@@ -14,15 +15,22 @@ app.get('/', function(req, res) {
 app.use('/', function(req, res) {
   // remove leading '/' from address
   var input = req.path.slice(1);
-  input = input.replace('%20', ' ');
+  input = replaceAll(input, '%20', ' ');
   var inputDate = new Date(input);
-  var unixTime = inputDate.getTime()/1000;
-  var naturalTime = getNamedMonth(inputDate.getMonth()) + ' ' + inputDate.getDay() + ', ' + inputDate.getFullYear();
+  if (inputDate == null) {
+    var unixTime = 'null';
+    var naturalTime = 'null';
+  }
+  else {
+    var unixTime = inputDate.getTime()/1000;
+    var naturalTime = getNamedMonth(inputDate.getMonth()) + ' ' + inputDate.getDay() + ', ' + inputDate.getFullYear();
+  }
   var timeOut = { 
     "unix" : unixTime,
     "natural" : naturalTime
   };
-  res.send(JSON.stringify(timeOut, null, '\t'));
+  res.json(timeOut);
+  //res.send(JSON.stringify(timeOut, null, '\t'));
 });
 
 const listener = app.listen(process.env.PORT, () => {
@@ -34,4 +42,8 @@ function getNamedMonth(monthNum) {
       'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'
     ];
   return months[monthNum];
+}
+
+function replaceAll(str, find, replace) {
+    return str.replace(new RegExp(find, 'g'), replace);
 }
