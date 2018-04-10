12d0d9aecbb21cd71eff975f1167b57f5bc1cb9c
Glitch (wise-wrench) <none>
2018-04-10 13:23:59 +0000

    🛩🏪 Checkpoint ./server.js:864828/472 ./package.json:864828/580

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 // grab the path information
 // check it for proper formatting
+// check the database for shortform link
+//   - if it exists, re-route to new link
+// check database for longform link
+//   - if the link doesn't exist, create a new one
+//   - if it does exist, output the two hyperlinks
 // if formatted properly, output two hyperlinks:
 //   - the input link
 //   - the shortened link
@@ -8,6 +13,8 @@
 var fs = require('fs');
 var express = require('express');
 var app = express();
+var mongo = require('mongodb').MongoClient;
+var url = 'mongodb://username:password@ds01316.mlab.com:1316/food'
 
 app.set('json spaces', 2);
 app.use(express.static('public'));
